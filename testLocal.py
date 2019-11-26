8336719b9afa10bd04d84f624e2a934126b2d44d
Timissky <31987959+Timissky@users.noreply.github.com>
2019-11-26 20:37:09 +0000

    Update testLocal.py

--- testLocal.py
+++ testLocal.py
@@ -2,7 +2,7 @@ import hashlib
 import time
 import boto3
 
-D = 5
+D = 7
 found = 0
 block = "COMSM0010cloud"
 nonce = 65536
@@ -10,12 +10,43 @@ i = -1
 sqs = boto3.client('sqs')
 URL = sqs.get_queue_url(QueueName='Task.fifo')
 queue_url = URL['QueueUrl']
+response = sqs.get_queue_url(QueueName='Result.fifo')
+resultURL = response['QueueUrl']
+
+
+def compute(k):
+    # i = i+1
+    x = hashlib.sha256()
+    y = hashlib.sha256()
+    code = block + str(k)
+    # code = block+"0000000000"
+    # print ("code= ",code)
+    x.update(code.encode("utf-8"))
+    temstr = x.hexdigest()
+    # print("temstr= ",temstr)
+
+    y.update(temstr.encode("utf-8"))
+    result = y.hexdigest()
+    return result
+
+# record the begin time
 
 checkstr = ""
 for j in range(0, D):
     checkstr = checkstr + "0"
 
-for receive in range(1, 20, 1):
+sqs.send_message(
+        QueueUrl=resultURL,
+        DelaySeconds=0,
+        MessageGroupId='begin',
+        MessageDeduplicationId='begin',
+        MessageAttributes={
+        },
+        MessageBody=(
+            "begin"
+        )
+    )
+for receive in range(1, 33, 1):
     if found == 1:
         break
     content = sqs.receive_message(
@@ -34,51 +65,44 @@ for receive in range(1, 20, 1):
     receipt_handle = message['ReceiptHandle']
     taskUnit = int(message['Body'])
 
-    for k in range(32768*(taskUnit-1)+1, 32768*taskUnit+1 ,1):
-        # i = i+1
-
-        x = hashlib.sha256()
-        y = hashlib.sha256()
-        code = block + str(k)
-        # code = block+"0000000000"
-        # print ("code= ",code)
-        x.update(code.encode("utf-8"))
-        temstr = x.hexdigest()
-        # print("temstr= ",temstr)
-
-        y.update(temstr.encode("utf-8"))
-        result = y.hexdigest()
-        # bin_result = hexstr2binstr(result)
-
-        # print("result= ", result)
-        # print("bin_reslut = ", bin_result)
-        # print("result[d] = ", result[0:2])
+    for num in range(2097152*(taskUnit-1)+1, 2097152*taskUnit+1, 1):
+        result = compute(num)
         if result[0:D] == checkstr:
             found = 1
+            print("nonce = %s" % num)
+            sqs.send_message(
+                QueueUrl=resultURL,
+                DelaySeconds=0,
+                MessageGroupId='1',
+                MessageDeduplicationId='1',
+                MessageAttributes={
+                },
+                MessageBody=(
+                    str(num)
+                )
+            )
             break
     sqs.delete_message(
         QueueUrl=queue_url,
         ReceiptHandle=receipt_handle
     )
     print('processed and deleted message: %s' % taskUnit)
-print("nonce = %s" % k)
-response = sqs.get_queue_url(QueueName='Result.fifo')
-resultURL = response['QueueUrl']
 sqs.send_message(
         QueueUrl=resultURL,
         DelaySeconds=0,
-        MessageGroupId = '1',
-        MessageDeduplicationId = '1',
+        MessageGroupId='end',
+        MessageDeduplicationId='end',
         MessageAttributes={
-            'Title': {
-                'DataType': 'Number',
-                'StringValue': str(k)
-            }
         },
         MessageBody=(
-            str(k)
+            "end"
         )
     )
+
+
+
+
+
 
 
 
